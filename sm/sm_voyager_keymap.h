ea6b8a82c05d34c0b4d2a87941f4642eeb52a51a
Stanislav Markin <stasmarkin@gmail.com>
2024-03-30 14:37:30 +0100

    swapped Э and З

--- sm/sm_voyager_keymap.h
+++ sm/sm_voyager_keymap.h
@@ -95,8 +95,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [L_RU] = LAYOUT_voyager(
     _______,   _______, SM_CYR_CC,  SM_CYR_U,  SM_CYR_K,  SM_CYR_E,         SM_CYR_N,  SM_CYR_G, SM_CYR_SH,SM_CYR_SCH,   _______,   _______,
-  SM_CYR_YY,  SM_CYR_F, SM_CYR_YI,  SM_CYR_V,  SM_CYR_A,  SM_CYR_P,         SM_CYR_R,  SM_CYR_O,  SM_CYR_L,  SM_CYR_D, SM_CYR_ZH, SM_CYR_EE,
-SM_CYR_SOLID,SM_CYR_YA, SM_CYR_CH,  SM_CYR_S,  SM_CYR_M,  SM_CYR_I,         SM_CYR_T,SM_CYR_SOFT, SM_CYR_B, SM_CYR_YU,  SM_CYR_Z,  SM_CYR_H,
+  SM_CYR_YY,  SM_CYR_F, SM_CYR_YI,  SM_CYR_V,  SM_CYR_A,  SM_CYR_P,         SM_CYR_R,  SM_CYR_O,  SM_CYR_L,  SM_CYR_D, SM_CYR_ZH,  SM_CYR_Z,
+SM_CYR_SOLID,SM_CYR_YA, SM_CYR_CH,  SM_CYR_S,  SM_CYR_M,  SM_CYR_I,         SM_CYR_T,SM_CYR_SOFT, SM_CYR_B, SM_CYR_YU, SM_CYR_EE,  SM_CYR_H,
     _______,   _______,   _______,   _______,   _______,   _______,          _______,   _______,   _______,   _______,   _______,   _______,
                                               CKC_SPACE, CKC_ENTER,          CKC_ESC,   CKC_TAB
   ),
