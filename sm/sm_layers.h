50c2bacf6d95cf77a82f9428d0aaa5bec196ca28
Stanislav Markin <stasmarkin@gmail.com>
2024-04-02 14:12:25 +0200

    add WIN and MAC symbols

--- sm/sm_layers.h
+++ sm/sm_layers.h
@@ -10,19 +10,61 @@ static uint16_t sm_layers_keycode_to_shortcut_row[SM_LAYERS_SIZE] = { NOT_INIT }
 static uint16_t sm_layers_keycode_to_shortcut_col[SM_LAYERS_SIZE] = { NOT_INIT };
 
 
-#define CASE_LOWER_UPPER_UNICODE(key, uc_l, uc_u)           \
-        case key:                                           \
-            if (get_oneshot_mods() != 0) {                  \
-                sm_layers_keycode_to_shortcut_tap(keycode);  \
-            } else if (get_mods() == MOD_BIT(KC_LSHIFT) ||  \
-                    get_mods() == MOD_BIT(KC_RSHIFT)) {     \
-                register_unicode(uc_u);                     \
-            } else if (get_mods() == 0) {                   \
+#define CASE_UC_LU(key, uc_l, uc_u)                         \
+        case key: {                                         \
+            uint8_t mods = get_mods() | get_oneshot_mods(); \
+            if (mods == 0) {                                \
                 register_unicode(uc_l);                     \
+            } else if (mods & ~MOD_MASK_SHIFT) {            \
+                sm_layers_keycode_to_shortcut_tap(keycode); \
             } else {                                        \
-                sm_layers_keycode_to_shortcut_tap(keycode);  \
+                register_unicode(uc_u);                     \
             }                                               \
             return false;                                   \
+        }
+
+
+#define CASE_UC_L(key, uc_l, kc_u)                          \
+        case key: {                                         \
+            uint8_t mods = get_mods() | get_oneshot_mods(); \
+            if (mods == 0) {                                \
+                register_unicode(uc_l);                     \
+            } else if (mods & ~MOD_MASK_SHIFT) {            \
+                sm_layers_keycode_to_shortcut_tap(keycode); \
+            } else {                                        \
+                tap_code16(kc_u);                           \
+            }                                               \
+            return false;                                   \
+        }
+
+
+#define CASE_UC_U(key, kc_l, uc_u)                          \
+        case key: {                                         \
+            uint8_t mods = get_mods() | get_oneshot_mods(); \
+            if (mods == 0) {                                \
+                tap_code16(kc_l);                           \
+            } else if (mods & ~MOD_MASK_SHIFT) {            \
+                sm_layers_keycode_to_shortcut_tap(keycode); \
+            } else {                                        \
+                register_unicode(uc_u);                     \
+            }                                               \
+            return false;                                   \
+        }
+
+
+#define CASE_BASIC(key, kc_l, kc_u)                      \
+        case key: {                                         \
+            uint8_t mods = get_mods() | get_oneshot_mods(); \
+            if (mods == 0) {                                \
+                tap_code16(kc_l);                           \
+            } else if (mods & ~MOD_MASK_SHIFT) {            \
+                sm_layers_keycode_to_shortcut_tap(keycode); \
+            } else {                                        \
+                register_unicode(kc_u);                     \
+            }                                               \
+            return false;                                   \
+        }
+
 
 void sm_layers_make_shortcut_tap(uint8_t row, uint8_t col) {
     uint8_t return_layer = get_highest_layer(layer_state);
@@ -71,39 +113,71 @@ void sm_layers_keycode_to_shortcut_tap(uint16_t sm_layers_keycode) {
 
 bool process_sm_layers_tap(uint16_t keycode) {
     switch (keycode) {
-        CASE_LOWER_UPPER_UNICODE(CYR_YY, 0x0439, 0x0419);
-        CASE_LOWER_UPPER_UNICODE(CYR_CC, 0x0446, 0x0426);
-        CASE_LOWER_UPPER_UNICODE(CYR_U, 0x0443, 0x0423);
-        CASE_LOWER_UPPER_UNICODE(CYR_K, 0x043A, 0x041A);
-        CASE_LOWER_UPPER_UNICODE(CYR_E, 0x0435, 0x0415);
-        CASE_LOWER_UPPER_UNICODE(CYR_F, 0x0444, 0x0424);
-        CASE_LOWER_UPPER_UNICODE(CYR_YI, 0x044B, 0x042B);
-        CASE_LOWER_UPPER_UNICODE(CYR_V, 0x0432, 0x0412);
-        CASE_LOWER_UPPER_UNICODE(CYR_A, 0x0430, 0x0410);
-        CASE_LOWER_UPPER_UNICODE(CYR_P, 0x043F, 0x041F);
-        CASE_LOWER_UPPER_UNICODE(CYR_YA, 0x044F, 0x042F);
-        CASE_LOWER_UPPER_UNICODE(CYR_CH, 0x0447, 0x0427);
-        CASE_LOWER_UPPER_UNICODE(CYR_SS, 0x0441, 0x0421);
-        CASE_LOWER_UPPER_UNICODE(CYR_M, 0x043C, 0x041C);
-        CASE_LOWER_UPPER_UNICODE(CYR_I, 0x0438, 0x0418);
-        CASE_LOWER_UPPER_UNICODE(CYR_N, 0x043D, 0x041D);
-        CASE_LOWER_UPPER_UNICODE(CYR_G, 0x0433, 0x0413);
-        CASE_LOWER_UPPER_UNICODE(CYR_SH, 0x0448, 0x0428);
-        CASE_LOWER_UPPER_UNICODE(CYR_SCH, 0x0449, 0x0429);
-        CASE_LOWER_UPPER_UNICODE(CYR_ZZ, 0x0437, 0x0417);
-        CASE_LOWER_UPPER_UNICODE(CYR_H, 0x0445, 0x0425);
-        CASE_LOWER_UPPER_UNICODE(CYR_R, 0x0440, 0x0420);
-        CASE_LOWER_UPPER_UNICODE(CYR_O, 0x043E, 0x041E);
-        CASE_LOWER_UPPER_UNICODE(CYR_YO, 0x0451, 0x0401);
-        CASE_LOWER_UPPER_UNICODE(CYR_L, 0x043B, 0x041B);
-        CASE_LOWER_UPPER_UNICODE(CYR_D, 0x0434, 0x0414);
-        CASE_LOWER_UPPER_UNICODE(CYR_YE, 0x044D, 0x042D);
-        CASE_LOWER_UPPER_UNICODE(CYR_T, 0x0442, 0x0422);
-        CASE_LOWER_UPPER_UNICODE(CYR_SFT, 0x044C, 0x042C);
-        CASE_LOWER_UPPER_UNICODE(CYR_B, 0x0431, 0x0411);
-        CASE_LOWER_UPPER_UNICODE(CYR_YU, 0x044E, 0x042E);
-        CASE_LOWER_UPPER_UNICODE(CYR_ZH, 0x0436, 0x0416);
-        CASE_LOWER_UPPER_UNICODE(CYR_SLD, 0x044A, 0x042A);
+        #ifdef SM_LAYERS_CYR_ENABLED
+        CASE_UC_LU(CYR_YY, 0x0439, 0x0419);
+        CASE_UC_LU(CYR_CC, 0x0446, 0x0426);
+        CASE_UC_LU(CYR_U, 0x0443, 0x0423);
+        CASE_UC_LU(CYR_K, 0x043A, 0x041A);
+        CASE_UC_LU(CYR_E, 0x0435, 0x0415);
+        CASE_UC_LU(CYR_F, 0x0444, 0x0424);
+        CASE_UC_LU(CYR_YI, 0x044B, 0x042B);
+        CASE_UC_LU(CYR_V, 0x0432, 0x0412);
+        CASE_UC_LU(CYR_A, 0x0430, 0x0410);
+        CASE_UC_LU(CYR_P, 0x043F, 0x041F);
+        CASE_UC_LU(CYR_YA, 0x044F, 0x042F);
+        CASE_UC_LU(CYR_CH, 0x0447, 0x0427);
+        CASE_UC_LU(CYR_SS, 0x0441, 0x0421);
+        CASE_UC_LU(CYR_M, 0x043C, 0x041C);
+        CASE_UC_LU(CYR_I, 0x0438, 0x0418);
+        CASE_UC_LU(CYR_N, 0x043D, 0x041D);
+        CASE_UC_LU(CYR_G, 0x0433, 0x0413);
+        CASE_UC_LU(CYR_SH, 0x0448, 0x0428);
+        CASE_UC_LU(CYR_SCH, 0x0449, 0x0429);
+        CASE_UC_LU(CYR_ZZ, 0x0437, 0x0417);
+        CASE_UC_LU(CYR_H, 0x0445, 0x0425);
+        CASE_UC_LU(CYR_R, 0x0440, 0x0420);
+        CASE_UC_LU(CYR_O, 0x043E, 0x041E);
+        CASE_UC_LU(CYR_YO, 0x0451, 0x0401);
+        CASE_UC_LU(CYR_L, 0x043B, 0x041B);
+        CASE_UC_LU(CYR_D, 0x0434, 0x0414);
+        CASE_UC_LU(CYR_YE, 0x044D, 0x042D);
+        CASE_UC_LU(CYR_T, 0x0442, 0x0422);
+        CASE_UC_LU(CYR_SFT, 0x044C, 0x042C);
+        CASE_UC_LU(CYR_B, 0x0431, 0x0411);
+        CASE_UC_LU(CYR_YU, 0x044E, 0x042E);
+        CASE_UC_LU(CYR_ZH, 0x0436, 0x0416);
+        CASE_UC_LU(CYR_SLD, 0x044A, 0x042A);
+        #endif
+
+        #ifdef SM_LAYERS_CYR_WIN_SYMBOLS_ENABLED
+        CASE_BASIC(CYR_W1, KC_1, KC_EXLM);
+        CASE_BASIC(CYR_W2, KC_2, KC_DQUO);
+        CASE_UC_U(CYR_W3, KC_3, 0x2116);
+        CASE_BASIC(CYR_W4, KC_4, KC_SCLN);
+        CASE_BASIC(CYR_W5, KC_5, KC_PERC);
+        CASE_BASIC(CYR_W6, KC_6, KC_COLN);
+        CASE_BASIC(CYR_W7, KC_7, KC_QUES);
+        CASE_BASIC(CYR_W8, KC_8, KC_ASTR);
+        CASE_BASIC(CYR_W9, KC_9, KC_LPRN);
+        CASE_BASIC(CYR_W0, KC_0, KC_RPRN);
+        CASE_BASIC(CYR_WDOT, KC_DOT, KC_COMMA);
+        CASE_BASIC(CYR_WSLASH, KC_BSLS, KC_SLSH);
+        #endif
+
+        #ifdef SM_LAYERS_CYR_MAC_SYMBOLS_ENABLED
+        CASE_BASIC(CYR_M1, KC_1, KC_EXLM);
+        CASE_BASIC(CYR_M2, KC_2, KC_DQUO);
+        CASE_UC_U(CYR_M3, KC_3, 0x2116);
+        CASE_BASIC(CYR_M4, KC_4, KC_PERC);
+        CASE_BASIC(CYR_M5, KC_5, KC_COLN);
+        CASE_BASIC(CYR_M6, KC_6, KC_COMMA);
+        CASE_BASIC(CYR_M7, KC_7, KC_DOT);
+        CASE_BASIC(CYR_M8, KC_8, KC_SCLN);
+        CASE_BASIC(CYR_M9, KC_9, KC_LPRN);
+        CASE_BASIC(CYR_M0, KC_0, KC_RPRN);
+        CASE_BASIC(CYR_MSLASH, KC_SLSH, KC_QUES);
+        CASE_BASIC(CYR_MBRC, KC_LBRC, KC_RBRC);
+        #endif
 
         default:
             return true;
