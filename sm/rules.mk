1b3664c694cb8235a19e5896b80815a10b5515c2
Stanislav Markin <stasmarkin@gmail.com>
2024-08-16 19:38:50 -0300

    debug and a fix

--- sm/rules.mk
+++ sm/rules.mk
@@ -1,7 +1,7 @@
 # Set any rules.mk overrides for your specific keymap here.
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
-CONSOLE_ENABLE = no
-COMMAND_ENABLE = no
+CONSOLE_ENABLE = yes #fixme-sm disable later
+COMMAND_ENABLE = yes #fixme-sm disable later
 MOUSEKEY_ENABLE = no
 ORYX_ENABLE = yes
 RGB_MATRIX_CUSTOM_KB = yes
